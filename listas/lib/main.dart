f1194c1f86c211dd47958bae2387973f1ea67af8
Rodrigo Saymon Fragoso Borges <rodrigotbass@gmail.com>
2022-07-22 10:13:22 -0300

    atualizar

--- listas/lib/main.dart
+++ listas/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:icofont_flutter/icofont_flutter.dart';
 import 'ciscle_avatar_custom.dart';
 
 void main() {
@@ -24,6 +24,7 @@ class CloneInsta extends StatelessWidget {
 class CloneInstaPage extends CloneInsta {
   @override
   Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         actions: [
@@ -38,66 +39,93 @@ class CloneInstaPage extends CloneInsta {
           ),
           SizedBox(width: 20),
           Icon(
-            Icons.chat_bubble_outline,
-            size: 30,
-          ),
+            IcoFontIcons.facebookMessenger, size: 30,),
+            
           SizedBox(width: 20),
         ],
-        backgroundColor: Colors.black,
-        title: const Text(
+         title: const Text(
           'Instagram',
           style: TextStyle(
             fontSize: 30,
             fontWeight: FontWeight.bold,
           ),
         ),
-      ),
-      body: Column(
-        children: [
-          SizedBox(height: 10),
-          Container(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+        backgroundColor: Colors.black,
+          ),
+
+        
+       
+      
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 10),
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  CiscleAvatarCustom(
+                    urlAvatar: 'https://www.w3schools.com/howto/img_avatar2.png',
+                    nome: 'Elã Borg...',
+                  ),
+                  CiscleAvatarCustom(
+                    urlAvatar: 'https://www.w3schools.com/howto/img_avatar.png',
+                    nome: 'Rodrigo S...',
+                  ),
+                  CiscleAvatarCustom(
+                    urlAvatar:
+                        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRqRyIiwYCq4s-fZi1zdmyfSuIPUvg9EyZ_Q&usqp=CAU',
+                    nome: 'Heitor',
+                  ),
+                  CiscleAvatarCustom(
+                    urlAvatar:
+                        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEMpUS3NxMp4i7QkSKNn40Oax_w-mLBFsoPA&usqp=CAU',
+                    nome: 'Julieta F...',
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 10),
+            ListTile(
+              title: Text(
+                'Rodrigo Saymon',
+                style: TextStyle(color: Colors.white),
+              ),
+              trailing: Icon(
+                Icons.more_horiz,
+                color: Colors.white,
+              ),
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    'https://www.w3schools.com/howto/img_avatar.png'),
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              color: Colors.teal,
+              // height: mediaQuery.height,
+              width: mediaQuery.width,
+              child: Image.network(
+                  'https://github.com/RodrigoSaymon/ListaDeTarefas/blob/main/assets/imagens/insta.png?raw=true'),
+            ),
+            SizedBox( height: 15,),
+
+            Row(
               children: [
-                CiscleAvatarCustom(
-                  urlAvatar: 'https://www.w3schools.com/howto/img_avatar2.png',
-                  nome: 'Elã Borg...',
-                ),
-                CiscleAvatarCustom(
-                  urlAvatar: 'https://www.w3schools.com/howto/img_avatar.png',
-                  nome: 'Rodrigo S...',
-                ),
-                CiscleAvatarCustom(
-                  urlAvatar:
-                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRqRyIiwYCq4s-fZi1zdmyfSuIPUvg9EyZ_Q&usqp=CAU',
-                  nome: 'Heitor',
-                ),
-                CiscleAvatarCustom(
-                  urlAvatar:
-                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEMpUS3NxMp4i7QkSKNn40Oax_w-mLBFsoPA&usqp=CAU',
-                  nome: 'Julieta F...',
-                ),
+                SizedBox( width: 20,),
+                Icon(Icons.favorite_border, color: Colors.white,size: 30,),
+                SizedBox( width: 20,),
+                Icon(Icons.chat_bubble_outline, color: Colors.white, size: 30,),
+                SizedBox( width: 20,),
+                Icon(Icons.near_me_outlined, color: Colors.white,size: 30,),
+                SizedBox( width: 20,),
+                Icon(Icons.turned_in_outlined, color: Colors.white,size: 30,),
               ],
             ),
-          ),
-          SizedBox(height: 10),
-          ListTile(
-            title: Text('Rodrigo Saymon', 
-            style: TextStyle(color: Colors.white),),
-            trailing: Icon(Icons.more_horiz, color: Colors.white,),
-            leading: CircleAvatar(
-              backgroundImage: NetworkImage(
-                  'https://www.w3schools.com/howto/img_avatar.png'),
-            ),
-          ),
-          Container(
-            color: Colors.teal,
-            height: 300,
-            width: 300,
-            child: Image.network('src'),
-          
-          )
-        ],
+            
+
+          ],
+        ),
       ),
     );
   }
